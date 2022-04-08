dad28985601e7e1c61b831690716a0723460e30a
justin6302971 <justin6302971@hotmail.com>
2022-04-08 12:43:19 +0800

    first commit for grpc example

---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // The port number(5001) must match the port of the gRPC server.
+            // This switch must be set before creating the GrpcChannel/HttpClient.
+            AppContext.SetSwitch(
+                "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            // The port number(5000) must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+        
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,11 @@
+# asp net core grpc
+
+
+## commands
+``` bash
+dotnet dev-certs https -ep ~/.aspnet/https/localapp.pfx -p temppassword -t
+```
+
+
+## references
+1. [asp net core grpc for mac](https://blog.yowko.com/aspdotnet-core-grpc-macos/)
